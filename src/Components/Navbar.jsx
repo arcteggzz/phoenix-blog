ec56eaac0c6859fe1cf67f0341e57b1ce3a471b5
Oghenetega Esedere <arcteggzz@gmail.com>
2023-02-28 13:51:10 +0100

    feat: added mobile navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,9 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import darkModeIcon from "../assets/images/darkModeIcon.png";
 import lightModeIcon from "../assets/images/lightModeIcon.png";
 import darkHamburger from "../assets/images/darkHamburger.png";
+import closeLight from "../assets/images/closeLight.png";
+import closeDark from "../assets/images/closeDark.png";
 import lightHamburger from "../assets/images/lightHamburger.png";
 import logo from "../assets/images/logo.png";
 import { ThemeContext, ToggleThemeContext } from "../App";
@@ -9,6 +11,7 @@ import { ThemeContext, ToggleThemeContext } from "../App";
 const NavBar = () => {
   const theme = useContext(ThemeContext);
   const toggleTheme = useContext(ToggleThemeContext);
+  const [navOpen, setNavOpen] = useState(false);
 
   return (
     <>
@@ -64,11 +67,47 @@ const NavBar = () => {
                 alt="website-theme-png"
               />
             </div>
-            <img
-              src={theme === "dark" ? darkHamburger : lightHamburger}
-              alt="hamburger-menu"
-            />
+            <div onClick={() => setNavOpen(true)}>
+              <img
+                src={theme === "dark" ? darkHamburger : lightHamburger}
+                alt="hamburger-menu"
+              />
+            </div>
           </div>
+          {/* mobile navigation */}
+          {navOpen ? (
+            <div
+              className={`lg:hidden md:hidden sm:flex sm:flex-col sm:absolute sm:top-0 sm:left-0 sm:h-screen sm:w-screen transition-transform duration-500 ease-in ${
+                theme === "dark" ? `sm:bg-bgDark` : `sm:bg-bgWhite`
+              }`}
+            >
+              <div
+                className="w-screen flex justify-end px-[24px] h-[72px] items-center"
+                onClick={() => setNavOpen(false)}
+              >
+                <img
+                  src={theme === "dark" ? closeDark : closeLight}
+                  alt="hamburger-menu"
+                />
+              </div>
+              <div className="lg:hidden md:hidden sm:flex sm:flex-col sm:items-center sm:justify-center sm:h-full sm:gap-12">
+                <p className="text-primary font-bold text-[18px] cursor-pointer">
+                  Home
+                </p>
+                <p className="text-subtleGrey font-normal text-[18px] cursor-pointer">
+                  About
+                </p>
+                <p className="text-subtleGrey font-normal text-[18px] cursor-pointer">
+                  Blog
+                </p>
+                <p className="text-subtleGrey font-normal text-[18px] cursor-pointer">
+                  Contact
+                </p>
+              </div>
+            </div>
+          ) : (
+            <></>
+          )}
         </div>
       </nav>
     </>
